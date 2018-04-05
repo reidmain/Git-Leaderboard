a94b2647ec4c64a2b2a7d5532db7515317377f77
Reid Main <reidmain@gmail.com>
2018-04-04 20:01:04 -0700

    Updated git_leaderboard to expose methods to allow generation of author summaries and how they are processed.

--- git_leaderboard.rb
+++ git_leaderboard.rb
@@ -31,6 +31,83 @@ class AuthorSummary
 	end
 end
 
+def author_summaries_for(
+	git_repository_path:,
+	normalized_names:,
+	banned_names:,
+	banned_paths:
+)
+	commits = commits_for_git_repo(git_repository_path,
+		normalized_names,
+		banned_names,
+		banned_paths)
+
+	author_summaries = {}
+
+	commits.each do |commit|
+		author_name = commit.author_name
+
+		author_summary = author_summaries[author_name]
+		if author_summary == nil
+			author_summary = AuthorSummary.new(author_name)
+			author_summaries[author_name] = author_summary
+		end
+
+		if commit.file_modifications.count > 0
+			author_summary.append(commit)
+		end
+	end
+
+	return author_summaries
+end
+
+def process(
+	author_summaries:,
+	output_path: nil,
+	verbose: false
+)
+	sorted_author_summaries_by_num_commits = author_summaries.values.sort { |x, y| y.commits <=> x.commits }
+	total_commits = sorted_author_summaries_by_num_commits.sum { |x| x.commits }
+	total_additions = sorted_author_summaries_by_num_commits.sum { |x| x.additions }
+	total_deletions = sorted_author_summaries_by_num_commits.sum { |x| x.deletions }
+	total_files_changed = sorted_author_summaries_by_num_commits.sum { |x| x.files_modified }
+
+	csv_file = nil
+	if output_path
+		csv_file = File.open(output_path, "w")
+		csv_file.write("Author,Commits,% of Commits,Additions,% of Additions,Deletions,% of Deletions,Files Changed,% of Files Changed")
+	end
+
+	sorted_author_summaries_by_num_commits.each do |author_summary|
+		author = author_summary.author_name
+		num_commits = author_summary.commits
+		num_additions = author_summary.additions
+		num_deletions = author_summary.deletions
+		num_files_changed = author_summary.files_modified
+
+		num_commits_percentage = (num_commits / total_commits.to_f * 100).round(2)
+		num_additions_percentage = (num_additions / total_additions.to_f * 100).round(2)
+		num_deletions_percentage = (num_deletions / total_deletions.to_f * 100).round(2)
+		num_files_changed_percentage = (num_files_changed / total_files_changed.to_f * 100).round(2)
+
+		if verbose
+			puts author
+			puts "\tCommits: #{num_commits} (#{num_commits_percentage}%)"
+			puts "\tAdditions: #{num_additions} (#{num_additions_percentage}%)"
+			puts "\tDeletions: #{num_deletions} (#{num_deletions_percentage}%)"
+			puts "\tFiles Changed: #{num_files_changed} (#{num_files_changed_percentage}%)"
+		end
+
+		if (csv_file)
+			csv_file.write("\n#{author},#{num_commits},#{num_commits_percentage},#{num_additions},#{num_additions_percentage},#{num_deletions},#{num_deletions_percentage},#{num_files_changed},#{num_files_changed_percentage}")
+		end
+	end
+
+	if (csv_file)
+		csv_file.close
+	end
+end
+
 class LeaderboardScriptOptions < CommitsScriptOptions
 	attr_reader :output_path
 
@@ -51,65 +128,16 @@ end
 if __FILE__ == $PROGRAM_NAME
 	script_options = LeaderboardScriptOptions.new(ARGV, OptionParser.new)
 
-	commits = commits_for_git_repo(script_options.git_repository_path,
-		script_options.normalized_names,
-		script_options.banned_names,
-		script_options.banned_paths)
+	author_summaries = author_summaries_for(
+		git_repository_path: script_options.git_repository_path,
+		normalized_names: script_options.normalized_names,
+		banned_names: script_options.banned_names,
+		banned_paths: script_options.banned_paths
+	)
 
-	author_summaries = {}
-
-	commits.each do |commit|
-		author_name = commit.author_name
-
-		author_summary = author_summaries[author_name]
-		if author_summary == nil
-			author_summary = AuthorSummary.new(author_name)
-			author_summaries[author_name] = author_summary
-		end
-
-		if commit.file_modifications.count > 0
-			author_summary.append(commit)
-		end
-	end
-
-	sorted_author_summaries_by_num_commits = author_summaries.values.sort { |x, y| y.commits <=> x.commits }
-	total_commits = sorted_author_summaries_by_num_commits.sum { |x| x.commits }
-	total_additions = sorted_author_summaries_by_num_commits.sum { |x| x.additions }
-	total_deletions = sorted_author_summaries_by_num_commits.sum { |x| x.deletions }
-	total_files_changed = sorted_author_summaries_by_num_commits.sum { |x| x.files_modified }
-
-	csv_file = nil
-	if script_options.output_path
-		csv_file = File.open(script_options.output_path, "w")
-		csv_file.write("Author,Commits,% of Commits,Additions,% of Additions,Deletions,% of Deletions,Files Changed,% of Files Changed")
-	end
-
-	sorted_author_summaries_by_num_commits.each do |author_summary|
-		author = author_summary.author_name
-		num_commits = author_summary.commits
-		num_additions = author_summary.additions
-		num_deletions = author_summary.deletions
-		num_files_changed = author_summary.files_modified
-
-		num_commits_percentage = (num_commits / total_commits.to_f * 100).round(2)
-		num_additions_percentage = (num_additions / total_additions.to_f * 100).round(2)
-		num_deletions_percentage = (num_deletions / total_deletions.to_f * 100).round(2)
-		num_files_changed_percentage = (num_files_changed / total_files_changed.to_f * 100).round(2)
-
-		if script_options.verbose
-			puts author
-			puts "\tCommits: #{num_commits} (#{num_commits_percentage}%)"
-			puts "\tAdditions: #{num_additions} (#{num_additions_percentage}%)"
-			puts "\tDeletions: #{num_deletions} (#{num_deletions_percentage}%)"
-			puts "\tFiles Changed: #{num_files_changed} (#{num_files_changed_percentage}%)"
-		end
-
-		if (csv_file)
-			csv_file.write("\n#{author},#{num_commits},#{num_commits_percentage},#{num_additions},#{num_additions_percentage},#{num_deletions},#{num_deletions_percentage},#{num_files_changed},#{num_files_changed_percentage}")
-		end
-	end
-
-	if (csv_file)
-		csv_file.close
-	end
+	process(
+		author_summaries: author_summaries,
+		output_path: script_options.output_path,
+		verbose: script_options.verbose
+	)
 end
