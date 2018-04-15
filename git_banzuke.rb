31a7b99452fce017384e1adc605b4e20e74b47c7
Reid Main <reidmain@gmail.com>
2018-04-15 15:53:15 -0700

    Updated git_banzuke to compute both the regular and raw banzukes.

--- git_banzuke.rb
+++ git_banzuke.rb
@@ -20,12 +20,41 @@ class BanzukeEntry
 	end
 end
 
+class Banzuke
+	attr_reader :author_summaries
+
+	def initialize()
+		@author_summaries = {}
+	end
+
+	def append(author_summaries_to_append)
+		for (author_name, author_summary) in author_summaries_to_append
+			if banzuke_author_summary = @author_summaries[author_name]
+				new_author_summary = AuthorSummary.new(
+					author_name: author_name,
+					number_of_commits: banzuke_author_summary.number_of_commits + author_summary.number_of_commits,
+					number_of_additions: banzuke_author_summary.number_of_additions + author_summary.number_of_additions,
+					number_of_deletions: banzuke_author_summary.number_of_deletions + author_summary.number_of_deletions,
+					number_of_files_modified: banzuke_author_summary.number_of_files_modified + author_summary.number_of_files_modified,
+					)
+
+				@author_summaries[author_name] = new_author_summary
+			else
+				@author_summaries[author_name] = author_summary
+			end
+		end
+	end
+end
+
 class BanzukeScriptOptions
 	attr_reader :entries
+	attr_reader :output_path
+	attr_reader :output_raw
 	attr_reader :verbose
 
 	def initialize(args, option_parser)
 		@entries = []
+		@output_raw = true
 		@verbose = true
 
 		option_parser.accept(JSON) do |option_json|
@@ -90,6 +119,24 @@ class BanzukeScriptOptions
 		end
 
 		option_parser.on(
+			"--output-path PATH",
+			String,
+			"The path that the script should save its output to.",
+			"The output will be a comma-separated values text file and as such will automatically have \".csv\" appended to it."
+			) do |output_path|
+				@output_path = output_path
+		end
+
+		option_parser.on(
+			"--output-raw BOOL",
+			"A switch to determine if the unfiltered leaderboard should also be outputted for every respository.",
+			"Defaults to true.",
+			TrueClass
+			) do |flag|
+				@verbose = flag
+		end
+
+		option_parser.on(
 			"--verbose BOOL",
 			"A switch to determine if actions taken should be outputted to the console.",
 			"Defaults to true.",
@@ -107,7 +154,8 @@ if __FILE__ == $PROGRAM_NAME
 
 	script_options = BanzukeScriptOptions.new(ARGV, OptionParser.new)
 
-	banzuke_author_summaries = {}
+	banzuke = Banzuke.new
+	raw_banzuke = Banzuke.new
 
 	for entry in script_options.entries do
 		if script_options.verbose
@@ -128,26 +176,39 @@ if __FILE__ == $PROGRAM_NAME
 			verbose: script_options.verbose
 		)
 
-		for (author_name, author_summary) in author_summaries
-			if banzuke_author_summary = banzuke_author_summaries[author_name]
-				new_author_summary = AuthorSummary.new(
-					author_name: author_name,
-					number_of_commits: banzuke_author_summary.number_of_commits + author_summary.number_of_commits,
-					number_of_additions: banzuke_author_summary.number_of_additions + author_summary.number_of_additions,
-					number_of_deletions: banzuke_author_summary.number_of_deletions + author_summary.number_of_deletions,
-					number_of_files_modified: banzuke_author_summary.number_of_files_modified + author_summary.number_of_files_modified,
-					)
+		banzuke.append(author_summaries)
 
-				banzuke_author_summaries[author_name] = new_author_summary
-			else
-				banzuke_author_summaries[author_name] = author_summary
+		if script_options.output_raw and entry.output_path.nil? == false
+			if script_options.verbose
+				puts "Computing the raw leaderboard for #{entry.git_repository_path}"
 			end
+
+			raw_author_summaries = author_summaries_for(
+				git_repository_path: entry.git_repository_path,
+				verbose: false
+			)
+
+			process(
+				author_summaries: raw_author_summaries,
+				output_path: "#{entry.output_path}_raw",
+				verbose: false
+			)
+
+			raw_banzuke.append(raw_author_summaries)
 		end
 	end
 
 	process(
-			author_summaries: banzuke_author_summaries,
-			output_path: "./_Banzuke",
+		author_summaries: banzuke.author_summaries,
+		output_path: script_options.output_path,
+		verbose: script_options.verbose
+	)
+
+	if script_options.output_raw and script_options.output_path.nil? == false
+		process(
+			author_summaries: raw_banzuke.author_summaries,
+			output_path: "#{script_options.output_path}_raw",
 			verbose: script_options.verbose
 		)
+	end
 end
